607e3aa72b021ed1f540357e2d0d1a59b9e33c8c
Maite <maite_azanza@estudante.sesisenai.org.br>
2025-07-18 16:52:25 -0300

    .

--- Formularios/php/phpexternoSA.php
+++ Formularios/php/phpexternoSA.php
@@ -1,12 +1,12 @@
 <?php
     if(isset($_post['num_comanda'])&& isset($_post['id_produto'])&& isset($_post['categoria_produto'])&& isset($_post['nome_produto'])&& isset($_post['quantidade'])&& isset($_post['preco'])&& isset($_post['unidade'])){
         echo "Dados dos produtos recebidos :<br>";
-        echo "Num: " . $_post['num_comanda'] . "<br>";
-        echo "CPF: " . $_post['id_produto'] . "<br>";
-        echo "Senha: " . $_post['categoria_produto'] . "<br>";
-        echo "Email: " . $_post['nome_produto'] . "<br>";
-        echo "Telefone: " . $_post['quantidade'] . "<br>";
-        echo "Rua: " . $_post['preco'] . "<br>";
-        echo "Número: " . $_post['unidade'] . "<br>";
+        echo "Número de Comanda: " . $_post['num_comanda'] . "<br>";
+        echo "ID do Produto: " . $_post['id_produto'] . "<br>";
+        echo "Categoria: " . $_post['categoria_produto'] . "<br>";
+        echo "Nome: " . $_post['nome_produto'] . "<br>";
+        echo "Quantidade: " . $_post['quantidade'] . "<br>";
+        echo "Preço: " . $_post['preco'] . "<br>";
+        echo "Unidade: " . $_post['unidade'] . "<br>";
     }
 ?>
